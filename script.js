1f8c2d36310979b43fa70c76e54814c30c03b08f
KrishnaGIMI <117573942+KrishnaGIMI@users.noreply.github.com>
2025-06-07 18:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	const hash = new Map();
-	for (let index = 0; index < str.length; index++) {
-		if(hash.has(str[i]){
-			hash.set(str[i])=hash.get(str[i])+1;
+	for (let i = 0; i < str.length; i++) {
+		if(hash.get(str[i])){
+			hash.set(str[i], hash.get(str[i])+1);
 		}
 		else{
-			hash.get(str[i])=1;
+			hash.set(str[i], 1);
 		}
 	}
-	for(let index = 0; index < str.length; index++){
+	for(let i = 0; i < str.length; i++){
 		if(hash.get(str[i])==1){
-			return str[i];
+			return str[i]; 
 		}
 	}
-	return "pl";
+	return null;
 }
-const input = prompt("Enter a string");
+const input = prompt("Enter a string ");
 alert(firstNonRepeatedChar(input)); 
+
+
+
